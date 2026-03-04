c4620f0994ebe5faf998a3eb0c2735ea180f10d3
siddu1205 <siddartha12052006@gmail.com>
2026-03-04 22:11:28 +0530

    feat: update routing structure and add new page components for Home, Contact, and Doctors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { browerRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <browerRouter>
     <App />
-  </StrictMode>,
+  </browerRouter>
 );
