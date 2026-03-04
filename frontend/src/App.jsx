2812d90010e9c81fc98cdfdd4f67af63a921a7af
siddu1205 <siddartha12052006@gmail.com>
2026-03-04 22:46:51 +0530

    feat: add routing and new page components for About, MyProfile, and MyAppointments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Profile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
 
 function App() {
   return (
-    <div className="text-green-500 text-3xl font-bold underline">
-      Testing 123
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+      </Routes>
     </div>
   );
 }
